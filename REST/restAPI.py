1d6dbc397ffa9b4529be0a184d7875ea64e5570a
jarpszon <jpszonczak@gmail.com>
2021-08-20 16:06:15 +0200

    added insert

--- REST/restAPI.py
+++ REST/restAPI.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import sqlalchemy as sqlal
 #import pyodbc
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, text
 import json
 
 # import create_engine
@@ -22,21 +22,26 @@ def test():
     return 'to jest test'
 
 
-@app.route('/name', methods=['GET'])
-def name():
+@app.route('/API/v1/getModelParameters', methods=['GET'])
+def getModelParameters():
     engine = sqlal.create_engine(ConStr)
 
     metadata = sqlal.MetaData(bind=None)
     table = sqlal.Table(
-        'Name',
+        'Model_Parameters',
         metadata,
         autoload=True,
-        autoload_with=engine
+        autoload_with=engine,
+        schema='models'
     )
 
     stmt = select([
-        table.columns.id,
-        table.columns.name]
+        table.columns.ID,
+        table.columns.Model,
+        table.columns.Parameter,
+        table.columns.Description ,
+        table.columns.Value ,
+        ]
     )
 
     connection = engine.connect()
@@ -50,7 +55,24 @@ def name():
 
     return json.dumps(a)
 
+@app.route('/API/v1/addModelParameter', methods=['POST'])
+def addModelParameter():
+    engine = sqlal.create_engine(ConStr)
+    
+    with engine.connect() as con:
+               
+        data = request.json
+        print(data)
+        statement = text("INSERT INTO models.Model_Parameters VALUES(:Model, :Parameter, :Description, :Value)")
+        
+        for line in data:
+            print(line)
+            con.execute(statement, **line)
+        
+    return 'OK'
+
+
 
 app.run(host='0.0.0.0',port=3000)
 
-#  use this in browser!   http://192.168.0.105:3000/name
+#  use this in browser!   http://192.168.0.105:3000/test
